388b04351c38f4a988a29e01c69a217e324ae00b
salehabdulla <salehabdullaict@gmail.com>
2026-07-26 09:40:58 +0300

    feat: add JPA entity mapping and validation annotations to Category

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,4 +1,23 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    @NotBlank
+    @NotNull
+    @Size(min = 3, max = 30, message = "category name size must be (Min 3, Max 30)")
+    private String categoryName;
 }
