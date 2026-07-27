5b52c580d0bda75de253332505d93112828e7671
salehabdulla <salehabdullaict@gmail.com>
2026-07-27 21:54:43 +0300

    Add Category JPA entity with Product relationship and validation

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,23 +1,30 @@
 package com.ecommerce.project.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import java.util.Set;
+
+@Entity(name = "categories")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
     @NotBlank
-    @NotNull
     @Size(min = 3, max = 30, message = "category name size must be (Min 3, Max 30)")
     private String categoryName;
+
+    @OneToMany(mappedBy = "category")
+    @JsonManagedReference
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Product> products;
 }
