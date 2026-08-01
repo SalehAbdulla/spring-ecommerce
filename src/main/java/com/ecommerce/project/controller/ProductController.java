f224d0d12a2734570664b502623417c99a7cdf8d
salehabdulla <salehabdullaict@gmail.com>
2026-08-01 14:36:06 +0300

    Add getProducts endpoint to retrieve products with pagination and sorting

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
 import com.ecommerce.project.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +22,15 @@ public class ProductController {
         ProductDTO createdProduct = productService.addProduct(categoryId, product);
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
+
+    @GetMapping("/public/products")
+    public ResponseEntity<ProductResponse> getProducts(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_ORDER, required = false) String sortOrder
+    ){
+        return new ResponseEntity<ProductResponse>(productService.getProducts(pageNumber, pageSize, sortBy, sortBy), HttpStatus.OK);
+    }
+
 }
