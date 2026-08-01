95707106a5e93c59c1012c22a75f8b8dccd41bd6
salehabdulla <salehabdullaict@gmail.com>
2026-08-01 14:36:13 +0300

    Rename categoryId to id in Category model for consistency

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long categoryId;
+    private Long id;
 
     @NotBlank
     @Size(min = 3, max = 30, message = "category name size must be (Min 3, Max 30)")
