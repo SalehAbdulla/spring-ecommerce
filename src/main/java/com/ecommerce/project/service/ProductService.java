f8a6d986be5d177895e793fa51817b6b1d15f9e6
salehabdulla <salehabdullaict@gmail.com>
2026-08-01 14:36:39 +0300

    Add getProducts method to ProductService interface for pagination support

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+import org.springframework.http.HttpStatusCode;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, ProductDTO productDTO);
+
+    ProductResponse getProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 }
