d718a7c7b75465d8861ab4d40b448b86da73649a
salehabdulla <salehabdullaict@gmail.com>
2026-08-02 17:29:55 +0300

    Add validation annotations to CategoryDTO

--- src/main/java/com/ecommerce/project/payload/CategoryDTO.java
+++ src/main/java/com/ecommerce/project/payload/CategoryDTO.java
@@ -1,5 +1,9 @@
 package com.ecommerce.project.payload;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
-    private long id;
+    private Long id;
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Min(3)
     private String categoryName;
 }
