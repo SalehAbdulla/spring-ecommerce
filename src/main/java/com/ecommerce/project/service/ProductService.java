2e5d854cce8f11a8854399282a1ee4bb52e3c5e8
salehabdulla <salehabdullaict@gmail.com>
2026-08-03 04:28:16 +0300

    Add updateProductImage method to ProductService

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -3,6 +3,9 @@ package com.ecommerce.project.service;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, ProductDTO productDTO);
@@ -10,6 +13,6 @@ public interface ProductService {
     ProductResponse getProductsByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     ProductResponse searchProductsByKeyword(String keyword, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     ProductDTO updateProduct(Long productId, ProductDTO productDTO);
-
     ProductDTO deleteProduct(Long productId);
+    ProductDTO updateProductImage(Long productId, MultipartFile multipartFile) throws IOException;
 }
